5c983c31caaadeead2f8a5c2c644df2c4a47ad63
AtsushiHosaka <hosaka2917@gmail.com>
2023-05-17 16:54:39 +0900

    Initial Commit

---
+++ test_lifecycle_text/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_lifecycle_text
+//
+//  Created by 保坂篤志 on 2023/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
